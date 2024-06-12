1b479416dcbdb62dfe09992a5a23982e637d34a6
Brian Bauska <brianbauska@gmail.com>
2024-06-12 12:29:32 -0700

    Update domtree.js

--- js/domtree.js
+++ js/domtree.js
@@ -100,3 +100,13 @@ function resize() {
     tree.style.top = ( ( window.innerHeight - height - 100 ) / 2 ) + 'px';
 }
 resize();
+
+      var _gaq = [['_setAccount', 'UA-15240703-1'], ['_trackPageview']];
+        (function(d, t) {
+          var g = d.createElement(t),
+              s = d.getElementsByTagName(t)[0];
+          g.async = true;
+          g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          s.parentNode.insertBefore(g, s);
+        })(document, 'script');
+
